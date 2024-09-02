bd4892dd06a1f9087af1110db1165ab789b745b8
KJNegrette24 <k.negrette@uniandes.edu.co>
2024-09-02 17:11:44 -0500

    Nuevos cambios

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -6,3 +6,35 @@ def new_list():
              'last': None,
              'size': 0,}
     return newlist
+
+def add_first(lista, elemento):
+    new_nodo = node.new_single_node(elemento)
+    if lista["size"] == 0:
+        new_nodo['first'] = new_nodo
+        new_nodo['last'] = new_nodo
+        return lista
+    else:
+        new_nodo['next'] = lista['first']
+        lista['first'] = new_nodo
+        return lista
+
+def add_last(lista, elemento):
+    new_nodo =node.new_single_node(elemento)
+
+    if lista["size"] == 0:
+        new_nodo['first'] = new_nodo
+        new_nodo['last'] = new_nodo
+        return lista
+    
+    else:
+        lista['last'] = new_nodo
+        return lista
+        
+def size(lista):
+    return lista["size"]
+
+def first_element(list):
+    if list["size"] == 0:
+        return None
+    else:
+        return list["first"]
